189ccb4d45a4f2c497ff17e53fbb1876e474c2ba
Acosta-Marcos <acostamarlih9@gmail.com>
2025-03-30 19:49:12 -0300

    Update urls.py

--- TP1/urls.py
+++ TP1/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import obtener_agregar_items, obtener_un_item, modificar_un_item, eliminar_un_item
+from .views import obtener_agregar_items, obtener_un_item, modificar_eliminar_item
 
 urlpatterns = [
 path('items/', obtener_agregar_items, name='obtener_agregar_items'),
 path('items/<int:id>', obtener_un_item, name='obtener_un_item'),
-path('items/modificar/<int:id>/<str:nombre>/', modificar_un_item, name='modificar_un_item'),
-path('items/eliminar/<int:id>', eliminar_un_item, name='eliminar_un_item'),
+path('items/modificar/<int:id>', modificar_eliminar_item, name='modificar_eliminar_item'),
 ]
